43ab6ab9cb432012ab0be3bfb6c704b2d7b7bdd0
Walter Nkosinathi Magagula <walter.magagula@bbd.co.za>
2024-04-30 18:29:55 +0200

    add initial sql files for flyway migration

---
+++ db/migrations/V002_populate_users_table.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (username, email) VALUES
+    ('user1', 'user1@example.com'),
+    ('user2', 'user2@example.com'),
+    ('user3', 'user3@example.com'),
+    ('user4', 'user4@example.com'),
+    ('user5', 'user5@example.com');
