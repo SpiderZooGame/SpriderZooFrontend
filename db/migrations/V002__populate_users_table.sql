aec7fa31f35932a0fa5537d7ee0a592867c4e85b
Walter Nkosinathi Magagula <walter.magagula@bbd.co.za>
2024-05-06 20:26:12 +0200

    update sql migration scripts

--- db/migrations/V002__populate_users_table.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO users (username, email) VALUES
-    ('user1', 'user1@example.com'),
-    ('user2', 'user2@example.com'),
-    ('user3', 'user3@example.com'),
-    ('user4', 'user4@example.com'),
-    ('user5', 'user5@example.com');
---
+++ db/migrations/V003__create_badges_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE badges (
+    badge_id SERIAL PRIMARY KEY,
+    badge_name VARCHAR(50) NOT NULL,
+    badge_description TEXT NOT NULL
+);
