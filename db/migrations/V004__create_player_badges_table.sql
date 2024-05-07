60326d3d9b8175da60527bc0207bc8892351efbd
Walter Nkosinathi Magagula <walter.magagula@bbd.co.za>
2024-05-07 18:51:20 +0200

    bug fix in sql script

--- db/migrations/V004__create_player_badges_table.sql
+++ db/migrations/V004__create_player_badges_table.sql
@@ -2,5 +2,5 @@ CREATE TABLE playerbadges (
     player_badge_id SERIAL PRIMARY KEY,
     date_awarded DATE NOT NULL,
     player_id INTEGER REFERENCES players(player_id),
-    badge_id INTEGER REFERENCES badges(badge_id),
+    badge_id INTEGER REFERENCES badges(badge_id)
 );
