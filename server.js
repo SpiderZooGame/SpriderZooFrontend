28268d8c11d90301fa941caa80409c1cff88065d
GerniVisser-bbd <Gerrit.Visser@bbd.co.za>
2024-05-08 17:30:33 +0200

    started dashboard styling

--- server.js
+++ server.js
@@ -8,12 +8,10 @@ app.use(express.json());
 
 app.use("/api/v1/users", userRoutes);
 
-// Serve static files
 app.use("/static", express.static(path.resolve(__dirname, "src", "static")));
 
 app.get("/views/:viewName", (req, res) => {
   const viewName = req.params.viewName;
-  // Read the HTML file from the views directory and send it as the response
   res.sendFile(path.resolve(__dirname, "src/static/html", `${viewName}.html`));
 });
 
--- src/static/js/index.js
+++ src/static/js/index.js
@@ -74,14 +74,13 @@ document.addEventListener("DOMContentLoaded", () => {
     router();
 });
 
+// Dark mode
 var darkMode = false;
 
-// default to system setting
 if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
 	darkMode = true;
 }
 
-// preference from localStorage should overwrite
 if (localStorage.getItem('theme') === 'dark') {
 	darkMode = true;
 } else if (localStorage.getItem('theme') === 'light') {
