d60c1e11961a7fbb0337146ab189816ade4975f4
GerniVisser-bbd <Gerrit.Visser@bbd.co.za>
2024-05-10 09:13:41 +0200

    remove access code

--- globalconfigs/constants.js
+++ globalconfigs/constants.js
@@ -5,7 +5,6 @@ const secrets = {
   CLIENT_ID: CLIENT_ID,
   CLIENT_SECRET: CLIENT_SECRET,
   GITHUB_AUTHCODE_REQUEST: `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=read:user,user:email`,
-  ACCESS_TOKEN_REQUEST: `https://github.com/login/oauth/access_token?client_id=${CLIENT_ID}&device_code=${REDIRECT_URL}&grant_type=urn:ietf:params:oauth:grant-type:device_code`,
 };
 
 module.exports = secrets;
